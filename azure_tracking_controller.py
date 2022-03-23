7668920147a3e576506008bee4a2d20d811cf12c
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-03-23 19:05:57 +0100

    Inserite variabili per connessione con Azure

--- azure_tracking_controller.py
+++ azure_tracking_controller.py
@@ -5,3 +5,10 @@ import random
 from azure.iot.device import MethodResponse
 from azure.iot.device.aio import IoTHubDeviceClient
 from azure.iot.device.aio import ProvisioningDeviceClient
+
+# VARIABILI PER LA CONNESSIONE CON AZURE (INPUT)
+security_type = "DPS"
+id_scope = "0ne005457FD"
+device_key = "4eRyI/FGYlEoirEXRU/Z3Jyz5BqNG7ldkvJLy7OXW2g="
+device_id = "2kv7cqlfibe"
+dps_endpoint = "global.azure-devices-provisioning.net"
