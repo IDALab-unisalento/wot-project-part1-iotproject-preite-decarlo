06f84722ca37d975ed6f6c9bceaebc5cba00e8d3
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2022-04-15 18:35:03 +0200

    Aggiornati parametri connessione con nuova cloud app

--- azure_tracking_controller.py
+++ azure_tracking_controller.py
@@ -14,9 +14,9 @@ logging.basicConfig(level=logging.ERROR)
 
 # VARIABILI PER LA CONNESSIONE CON AZURE (INPUT)
 security_type = "DPS"
-id_scope = "0ne005457FD"
-device_key = "4eRyI/FGYlEoirEXRU/Z3Jyz5BqNG7ldkvJLy7OXW2g="
-device_id = "2kv7cqlfibe"
+id_scope = "0ne00595EB7"
+device_key = "u4RcdA8gqr12Mq3xsNSjOYTMEpMzMlcfzGVL9ka2LUw="
+device_id = "257gga9780e"
 dps_endpoint = "global.azure-devices-provisioning.net"
 
 
